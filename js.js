ed814eaa721e89fbfb2efffec72b613dc5db70be
chenqizhong <114143340+chenqizhong@users.noreply.github.com>
2022-10-21 16:03:25 +0800

    feat:學習到function基礎運用

--- js.js
+++ js.js
@@ -1,7 +1,31 @@
 // 註解
-
+/*
+//計算機
 var num1=prompt("請輸入數字1");
 var num2=prompt("請輸入數字2");
 num1=parseInt(num1);
 num2=parseInt(num2);
 document.write(num1+num2);
+
+//這是一樣的意思
+var num1=prompt("請輸入數字1");
+var num2=prompt("請輸入數字2");
+document.write(Math.round(+num1)+(+num2));
+*/
+
+/*
+var ohho=[20,30,40,50,"瓜瓜"];
+ohho[4]="50";
+document.write(ohho.length);
+*/
+
+
+function add(qty1,qty2){
+    document.write(qty1 + qty2)
+    return  "喵喵";
+}
+
+value=add(5,2)
+document.write(value);
+
+
